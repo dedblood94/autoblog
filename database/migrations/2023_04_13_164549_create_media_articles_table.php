ce0166482407f401c2908fa412d22e7b43e5b3e5
dedblood94 <dedblood94@gmail.com>
2023-04-13 20:02:06 +0300

    Start project

---
+++ database/migrations/2023_04_13_164549_create_media_articles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media_articles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('article_id');
+            $table->string('filename');
+            $table->string('filepath');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media_articles');
+    }
+};
