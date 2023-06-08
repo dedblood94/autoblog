3542ac9cf9826d9e643d5e91169d437072a897ae
dedblood94 <dedblood94@gmail.com>
2023-06-08 14:50:11 +0300

    Done add contacts

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Контакти - Автоблог</title>
+    <link rel="stylesheet" type="text/css" href="../css/app.css">
+</head>
+<body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+@include('section.header') <h1>Контакти</h1>
+    <p>Свяжітся з нами:</p>
+    <ul>
+        <li>Email: info@example.com</li>
+        <li>Телефон: +1234567890</li>
+        <li>Адресса: г. Ваш город, ул. Ваша вулиця</li>
+    </ul>
+
+@include('section.footer')
+<script src="../../resources/js/app.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,14 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticlesController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 // Отображение главной
 Route::get('/', function () {
     return view('home');
 })->name('home');
+Route::get('contact',function (){
+    return view('contact');
+})->name('contact');
 // Отображение формы авторизации
 Route::get('login', 'App\Http\Controllers\AuthController@showLoginForm')->name('login');
 
